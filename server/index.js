d44842c49a7e62e8893c9724af49da98e28829c3
name <gowluonade@gmail.com>
2023-10-16 16:15:27 -0500

    insert sqlite

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import { createClient } from '@libsql/client'
 import express from 'express'
 import logger from 'morgan'
 import { createServer } from 'node:http'
@@ -5,7 +6,9 @@ import { Server } from 'socket.io'
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
 io.on('connection', (socket) => {
   console.log('a user connected')
@@ -14,11 +17,32 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:content)',
+        args: { content: msg }
+      })
+    } catch (error) {
+      console.log('🚀 ~ file: index.js:25 ~ socket.on ~ error:', error)
+      return
+    }
     console.log('message: ' + msg)
-    io.emit('chat message', msg)
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
   })
 })
+
+const db = createClient({
+  url: 'libsql://large-american-eagle-devdariill.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL
+  )`)
 
 app.use(logger('dev'))
 

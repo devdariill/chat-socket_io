1f788553f74e2cf15a5106fe1bdbe037a3b148ee
name <gowluonade@gmail.com>
2023-10-16 15:31:26 -0500

    clg msg server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,31 @@
 import express from 'express'
 import logger from 'morgan'
-
-const PORT = process.env.PORT || 3000
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
 
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-  res.send(process.cwd() + '/client/index.html')
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
 })
